52ccd3fc33ea8346ffee8bd314e9c036e522ba54
Roman <roman@react.com>
2021-08-14 20:23:16 +0300

    54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,9 +3,7 @@ import User from "./User/User";
 import axios from "axios";
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items);
         })
